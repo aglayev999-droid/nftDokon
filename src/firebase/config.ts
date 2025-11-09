e2705fe211c928b212a72f0caef903ae417b6fc9
Davron Aglayev <aglayev999@gmail.com>
2025-11-09 06:07:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4584099704-9da0a",
+  "appId": "1:927790516789:web:34c168d0872f444ef23f7c",
+  "apiKey": "AIzaSyDRQwP0gFj3shUesWL-XwnQDVhTYAdlpPY",
+  "authDomain": "studio-4584099704-9da0a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "927790516789"
+};
