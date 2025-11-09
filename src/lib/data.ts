c595df487a4988f791c93c607f1d8445c1e8383b
Davron Aglayev <aglayev999@gmail.com>
2025-11-09 12:51:51 +0000

    qara auksion inventar bozor hammasidagi giftlarni obtasha

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -29,36 +29,7 @@ export interface Nft {
   startingPrice?: number;
 }
 
-export const nftsData: Nft[] = [
-  {
-    id: 'moon-pendant-2075',
-    name: 'Moon Pendant',
-    price: 250000,
-    rarity: 'Legendary',
-    collection: 'TON Treasures',
-    model: 'Epic',
-    background: 'Space',
-    imageUrl: 'https://nft.fragment.com/gift/MoonPendant-2075.jpg',
-    lottieUrl: 'https://nft.fragment.com/gift/MoonPendant-2075.lottie.json',
-    imageHint: 'moon pendant',
-    isListed: true,
-    ownerId: 'user-3' // Not the current user
-  },
-  {
-    id: 'fresh-socks-91000',
-    name: 'Fresh Socks',
-    price: 91000,
-    rarity: 'Epic',
-    collection: 'TON Treasures',
-    model: 'Epic',
-    background: 'Neon',
-    imageUrl: 'https://nft.fragment.com/gift/FreshSocks-91000.jpg',
-    lottieUrl: 'https://nft.fragment.com/gift/FreshSocks-91000.lottie.json',
-    imageHint: 'fresh socks',
-    isListed: false, // Start as unlisted so we can auction it
-    ownerId: 'user-1' // Belongs to our test user
-  }
-];
+export const nftsData: Nft[] = [];
 
 // Auction data is now managed in Firestore, so this can be empty.
 export const auctionNfts: Nft[] = [];
